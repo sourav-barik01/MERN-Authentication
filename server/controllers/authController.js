7b51da94827646591672613b70cb6547509815f3
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-10-30 10:55:55 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -79,8 +79,8 @@ export const login = async (req, res) => {
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? "none" : "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7*24*60*60*1000     // 7 Days in ms
         })
         return res.json({
